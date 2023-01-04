5697724a6c435d9d7759f52e7a80c3417d2aee71
Harshupadhyay2905 <29harshupadhyay052003@gmail.com>
2023-01-04 12:46:19 +0530

    first commit

---
+++ app/src/main/java/com/example/trivago/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.trivago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView menu;
+    ImageView login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        menu=findViewById(R.id.menu);
+        login=findViewById(R.id.imageView13);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
